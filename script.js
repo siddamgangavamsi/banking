89224c1e81333450d57fc8666134559839ddca1d
siddamgangavamsi <90108935+siddamgangavamsi@users.noreply.github.com>
2023-08-14 16:18:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
-function sendMoney(){
-    var enterName = document.getElementById("enterName").value;
-    var enterAmount = parseInt(document.getElementById("enterAmount").value);
- var enterSName = document.getElementById("enterSName").value;
-  var findSenderBankAccount = enterSName + "BankBalance";
-    var enterSAmount =parseInt(document.getElementById(findSenderBankAccount).innerHTML);
-    if (enterAmount > enterSAmount) {
-       alert("Insufficient Balance.")
-    }
- else {
-       var findUserBankAccount = enterName + "BankBalance";
-      
-       var finalAmount = parseInt(document.getElementById(findUserBankAccount).innerHTML) + enterAmount;
-       var myAccountBalance = parseInt(document.getElementById(findSenderBankAccount).innerHTML) - enterAmount;
-       document.getElementById(findSenderBankAccount).innerHTML = myAccountBalance;
-       document.getElementById(findUserBankAccount).innerHTML = finalAmount;
-       alert(`Successful Transaction !!  
-       Rs ${enterAmount} is sent to ${enterName}@gmail.com.`)
- 
-       // transaction history 
-       var createPTag = document.createElement("li");
-       var textNode = document.createTextNode(`Rs ${enterAmount} is sent from the sender with Email-id ${enterSName}@gmail.com
-  to recepient with Email-id ${enterName}@gmail.com on ${Date()}.`);
-       createPTag.appendChild(textNode);
-       var element = document.getElementById("transaction-history-body");
-       element.insertBefore(createPTag, element.firstChild);
-    }
- }
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let text = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML== '=') {
+            text = eval(text);
+            input.value = text;
+        }
+        else if (e.target.innerHTML == 'DEL') {
+            text = text.substring(0, text.length - 1);
+            input.value = text;
+        }
+        else if (e.target.innerHTML == 'AC') {
+            text = "";
+            input.value = text;
+        }
+        else
+            text += e.target.innerHTML;
+        input.value = text;
+
+
+    })
+})
